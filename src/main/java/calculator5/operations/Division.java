b0aa6bf52873bb6d61ebb18aa1c923840111dea8
Svetlana Kuchumova <svetikpodv@yandex.ru>
2021-03-23 00:38:31 +0500

    Calculator OOP (Task 5) + Exceptions (Task 7)

--- src/main/java/calculator5/operations/Division.java
+++ src/main/java/calculator5/operations/Division.java
@@ -2,7 +2,17 @@ package calculator5.operations;
 
 public class Division {
 
-    public Division(double x, double y) {
-        System.out.printf("%.4f / %.4f = %.4f ", x, y, x / y);
+    public Division(double x, double y) throws ArithmeticException {
+        double res = x / y;
+
+        if (y == 0) {
+            try {
+                throw new java.lang.ArithmeticException();
+            } catch (ArithmeticException e) {
+                System.out.println("На ноль делить нелья!");
+            }
+        } else {
+            System.out.printf("%.4f / %.4f = %.4f ", x, y, res);
+        }
     }
 }
